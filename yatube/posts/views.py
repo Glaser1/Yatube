94ec68b7bf91caea667223b8bd08f27e3ed2dbd1
unknown <sashapliskov@gmail.com>
2021-11-28 07:41:45 +0300

    Corrected

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -118,7 +118,11 @@ def follow_index(request):
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
     if request.user != author:
-        Follow.objects.create(user=request.user, author=author)
+        if len(Follow.objects.filter(
+                user=request.user,
+                author=author
+        ).count() != 0):
+            Follow.objects.create(user=request.user, author=author)
     return redirect('posts:profile', author)
 
 
